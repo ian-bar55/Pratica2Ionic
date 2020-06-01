9bccfff1e70afcf8a7c09e4b1c7f68da47c6839a
unknown <iianfernandess@gmail.com>
2020-06-01 00:01:20 -0300

    Tentativa de fazer a pratica

--- Pratica2Ionic/src/app/posts/posts.page.ts
+++ Pratica2Ionic/src/app/posts/posts.page.ts
@@ -13,11 +13,48 @@ import { PostsService } from '../services/posts.service';
 
 export class PostsPage implements OnInit {
   posts: any;
-  progressFlag: boolean;
+  valordabarra: number;
+  exibeBarra: boolean;
+  private index: number = 0;
+  private readonly offset: number = 15;
+  postsPage: any[];
 
-  constructor(private postService: PostsService) { }
-
-  ngOnInit() {
+  constructor(private postService: PostsService) { 
+    this.inicializarBarraProgresso;
+    this.countdownBarraProgresso;
   }
 
+  async ngOnInit() {
+    let resposta = await this.postService.getPosts();
+    this.posts = resposta;
+    this.postsPage = this.posts.slice(this.index,this.offset+this.index);
+    this.index += this.offset;
+
+  }
+
+  inicializarBarraProgresso(){
+    this.exibeBarra = true;
+    setInterval(() => {
+      this.valordabarra += 0.25;
+    }, 500)
+  }
+  countdownBarraProgresso(){
+    setTimeout(() => {
+      this.exibeBarra = false;
+    }, 2000)
+  }
+  loadData(event) {
+    setTimeout(() => {
+      let newPosts = this.posts.slice(this.index,this.offset+this.index);
+      this.index += this.offset;
+      
+      for (let i=0;i<newPosts.length;i++) {
+        this.postsPage.push(newPosts[i]);
+      }
+      event.target.complete();
+      if (this.postsPage.length == this.posts.length) {
+        event.target.disabled = true;
+      }
+    }, 500);
+  }
 }
